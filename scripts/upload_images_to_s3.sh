363da40c3181d5a6ad523ffed14b51cdbea21677
unknown <sh1517.you@gmail.com>
2024-07-07 22:58:02 +0900

    update script

--- scripts/upload_images_to_s3.sh
+++ scripts/upload_images_to_s3.sh
@@ -4,8 +4,9 @@
 TOKEN=$(curl --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 3600")
 ACCOUNT_ID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document --header "X-aws-ec2-metadata-token: $TOKEN" | jq -r .accountId)
 
+# Relative path based on the Cloud9 'environment' directory
 BUCKET_NAME="lab-edu-bucket-image-$ACCOUNT_ID"
-FOLDER_PATH="../images/animal_picture/"
+FOLDER_PATH="cloud-wave-workspace/images/animal_picture/"
 
 aws s3 mb "s3://$BUCKET_NAME"
 aws s3 cp "$FOLDER_PATH" "s3://$BUCKET_NAME/" --recursive
